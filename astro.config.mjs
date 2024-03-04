4cbad137052e3895eb8264ed05c5f0485482e4f6
jasperyzh <jasper.yzh@gmail.com>
2024-03-04 15:08:38 +0800

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  projectRoot: "./",
+  publicPath: "/staging.fishermen-analytics.com/goodday/",
+  dist: "./dist",
+});
